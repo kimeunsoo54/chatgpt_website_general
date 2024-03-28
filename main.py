175be33bb487082a1e6d845b8b4d2c19afe0a2e3
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-29 00:48:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -71,15 +71,16 @@ def login():
 
 if st.button("회원가입"):
     with st.form("form1", clear_on_submit = True):
-    name_student = st.text_input("학생 성함을 입력해주세요", key=1)
-    phone_student = st.text_input("학생 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=2)
-    name_parent = st.text_input("학부모님 성함을 입력해주세요", key=3)
-    phone_parent = st.text_input("학부모님 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=4)
-    id = st.text_input("아이디를 입력해주세요", key=5)
-    password = st.text_input("비밀번호를 입력해주세요", key=6, type="password")
-    password_check = st.text_input("비밀번호를 한 번 더 입력해주세요", key=7)
+        name_student = st.text_input("학생 성함을 입력해주세요", key=1)
+        phone_student = st.text_input("학생 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=2)
+        name_parent = st.text_input("학부모님 성함을 입력해주세요", key=3)
+        phone_parent = st.text_input("학부모님 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=4)
+        id = st.text_input("아이디를 입력해주세요", key=5)
+        password = st.text_input("비밀번호를 입력해주세요", key=6, type="password")
+        password_check = st.text_input("비밀번호를 한 번 더 입력해주세요", key=7)
 
-    submit = st.form_submit_button("작성 완료")
+        submit = st.form_submit_button("작성 완료")
+
 
 if st.button("로그인"):
     login()
