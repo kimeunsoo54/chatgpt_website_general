3fc60a8d617bdc9cf7e3e8cd3f38f158e382d902
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-29 00:23:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,12 +25,19 @@ import time
 
 def signup():
   name_student = st.text_input("학생 성함을 입력해주세요", key=1)
+  st.write(name_student)
   phone_student = st.text_input("학생 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=2)
+  st.write(phone_student)
   name_parent = st.text_input("학부모님 성함을 입력해주세요", key=3)
+  st.write(name_parent)
   phone_parent = st.text_input("학부모님 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=4)
+  st.write(phone_parent)
   id = st.text_input("아이디를 입력해주세요", key=5)
-  password = st.text_input("비밀번호를 입력해주세요", key=6)
+  st.write(id)
+  password = st.text_input("비밀번호를 입력해주세요", key=6, type="password")
+  st.write(len(password), "글자의 비밀번호입니다")
   password_check = st.text_input("비밀번호를 한 번 더 입력해주세요", key=7)
+  st.write(len(password_check), "글자의 비밀번호입니다")
 
 
   if st.button("완료"):
@@ -44,7 +51,9 @@ def signup():
 
 def login():
   id = st.text_input("아이디")
-  password = st.text_input("비밀번호")
+  st.write(id)
+  password = st.text_input("비밀번호", type="password")
+  st.write(len(password), "글자입니다")
 
   
 
