91a06e25e9cd572eb90dadcd8a0af3a4ffce0e8c
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-28 20:14:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import streamlit as st
 st.text('hello streamlit')
 
+#홈페이지 꾸미기 
+#html = 
+#st.markdown(html, unsafe_allow_html=True)
+
+member_name = ['김은수']
+member_phone = ['01039109020']
+
+# 리스트 요소 추가하는 함수 (새로운사람이 들어왔을 때)
+# 따로 db를 만들자 (java script, html이 서로 호환됨)
+
+
+def verification(name, phone):
+  if member_name.index(name) == member_name.index(phone):
+
+  if name not in member_name or phone not in member_phone:
+    return "결제가 완료되지 않은 상태입니다"
+
+
