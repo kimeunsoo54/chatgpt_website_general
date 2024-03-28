5fe73efa6233d573b132d5d204a84ca844a263da
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-29 03:57:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def signup():
 
 
 def login():
-    with st.form("form1", clear_on_submit = True):
+    with st.form("form2", clear_on_submit = True):
         id = st.text_input("아이디")
         password = st.text_input("비밀번호", type="password")
 
