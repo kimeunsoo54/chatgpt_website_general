b4523644951940f9148a68bafa663217fae083ec
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-29 14:42:44 +0900

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,1 +1,8 @@
+def login():
+    with st.form("form2", clear_on_submit = True):
+        id = st.text_input("아이디")
+        password = st.text_input("비밀번호", type="password")
 
+        submit = st.form_submit_button("로그인")
+
+login()
