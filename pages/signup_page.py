3795d3ab215c21af5727036cb542d0c165f767ff
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-29 22:34:30 +0900

    Update signup_page.py

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -4,7 +4,7 @@ import csv
 
 
 def signup():
-	with st.form("form1", clear_on_submit = True):
+	with st.form("form1", clear_on_submit = False):
 		name_student = st.text_input("학생 성함을 입력해주세요", key=1)
 		phone_student = st.text_input("학생 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=2)
 		name_parent = st.text_input("학부모님 성함을 입력해주세요", key=3)
@@ -15,7 +15,7 @@ def signup():
 		submit = st.form_submit_button("작성 완료")
 
 		if submit == True:
-			f = open("pages/member_free.CSV", 'w', newline='')
+			f = open("pages/member_free.csv", 'a', newline='')
 			wr = csv.writer(f)
 			wr.writerow([name_student, phone_student, id, password, 3])
 			f.close()
