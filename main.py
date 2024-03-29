92ba789238224cbbdaf4036e29318cf4da7b626e
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-29 13:44:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ page_names_to_funcs[page_name]()
 
 
 if st.button("회원가입"):
-    st.page_link("pages/signup_page.py")
+    st.switch_page("pages/signup_page.py")
 
                  
 #csv 파일에 이미 있는 사람이면 재가입 못하도록 접근 막아야 한다 (학생, 학부모 정보로 판단)
