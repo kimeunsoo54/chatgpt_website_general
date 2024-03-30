c366783cd823e7fca9f8db0bd22b06b5520ccebe
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-31 01:47:02 +0900

    signup_page.py 업데이트

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -28,7 +28,7 @@ def signup():
 			
 			for a in read:
 				if a[0] == name_student:
-					st.switch_page("pages/chatgpt.py, lable = "세특 GPT")
+					st.switch_page("pages/chatgpt.py, label = "세특 GPT")
 					
 					
 			f_.close()
