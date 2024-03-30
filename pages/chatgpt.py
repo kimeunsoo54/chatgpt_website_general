52bb47bec351a5d753bb6554ab2fbf04e57f3986
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-31 01:58:43 +0900

    chatgpt.py 업데이트

--- pages/chatgpt.py
+++ pages/chatgpt.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.components.v1.iframe("https://chat.openai.com/g/g-eKLEGdnCA-seteugeul-yejihada", height=1000, scrolling=True)
 
-st.page_link("https://chat.openai.com/g/g-eKLEGdnCA-seteugeul-yejihada")
+#st.page_link("https://chat.openai.com/g/g-eKLEGdnCA-seteugeul-yejihada")
