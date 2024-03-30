0e3cc108aac7eeffb1231324a8c79b13e8aefe35
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-03-30 14:51:11 +0900

    Update signup_page.py

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -5,7 +5,7 @@ import csv
 
 def signup():
 	
-	
+	"""
 	if 'id' not in st.session_state:
 		st.session_state['id']=''
 
@@ -14,25 +14,26 @@ def signup():
 
 	if 'number' not in st.session_state:
 		st.session_state['number']=''
-
+		
+	if 'name_student' not in st.session_state:
+    		st.session_state['name_student'] = ''
+		
+	if 'phone_student' not in st.session_state:
+    		st.session_state['phone_student'] = ''
+	"""
 	
 	
 	with st.form("form1", clear_on_submit = False):
-		if 'name_student' not in st.session_state:
-    			st.session_state['name_student'] = ''
 		
-		if 'phone_student' not in st.session_state:
-    			st.session_state['phone_student'] = ''
-
 
 		
 		name_student = st.text_input("학생 성함을 입력해주세요", key=1)
-		if name_student:
-			st.session_state['name_student'] = name_student
+		#if name_student:
+			#st.session_state['name_student'] = name_student
 		
 		phone_student = st.text_input("학생 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=2)
-		if phone_student:
-			st.session_state['phone_student'] = phone_student
+		#if phone_student:
+			#st.session_state['phone_student'] = phone_student
 		
 		name_parent = st.text_input("학부모님 성함을 입력해주세요", key=3)
 		phone_parent = st.text_input("학부모님 전화번호를 입력해주세요 (띄어쓰기, 하이픈(-) 제외하고 숫자만)", key=4)
@@ -44,7 +45,7 @@ def signup():
 		if submit == True:
 			f = open("pages/files/member_free.CSV", 'a', newline='')
 			wr = csv.writer(f)
-			wr.writerow([st.session_state['name_student'], st.session_state['phone_student']])
+			wr.writerow([name_student, phone_student, id, password, 3])
 			f.close()
 			st.switch_page("pages/login_page.py")
 
