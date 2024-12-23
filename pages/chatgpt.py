a8c3a2a8089aab7aa79dd4f1ffcf407c21b9edca
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-12-23 17:53:03 +0900

    Update chatgpt.py

--- pages/chatgpt.py
+++ pages/chatgpt.py
@@ -1,3 +1,6 @@
+!pip install --upgrade openai
+!pip install --upgrade langchain
+
 import streamlit as st
 
 #st.page_link("https://chat.openai.com/g/g-eKLEGdnCA-seteugeul-yejihada", label = 'yakdaeman')
