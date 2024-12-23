40e9fe0f08bcedf1609a27ca886dab6fcfe5c36d
Kim Eunsoo <97098328+kimeunsoo54@users.noreply.github.com>
2024-12-23 18:03:48 +0900

    Update chatgpt.py

--- pages/chatgpt.py
+++ pages/chatgpt.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
-#st.page_link("https://chat.openai.com/g/g-eKLEGdnCA-seteugeul-yejihada", label = 'yakdaeman')
-
-
-#st.components.v1.iframe("https://chat.openai.com/g/g-eKLEGdnCA-seteugeul-yejihada", height=1000, scrolling=True)
-
-#st.page_link("https://chat.openai.com/g/g-eKLEGdnCA-seteugeul-yejihada")
-
 import langchain
 import openai
 import os
